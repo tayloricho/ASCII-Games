6830fbd3386d69d3f1680bb99e090bbc005941e7
taylorricho <taylorlloydrichardson@gmail.com>
2023-04-15 17:14:32 +0100

    Connect 4

--- Y12 Easter Project - ASCII Games/ConnectFour.java
+++ Y12 Easter Project - ASCII Games/ConnectFour.java
@@ -1,29 +1,115 @@
-class ConnectFour extends Game {
-  
-  //Variable declarations go here. Standard connect 4 has a 6x7 board and the pieces are red (R) or black (B).
+import java.util.Scanner;
 
-  public ConnectFour(){
+class ConnectFour extends Game {
+
+  // Variable declarations go here. Standard connect 4 has a 6x7 board and the
+  // pieces are red (R) or black (B).
+
+  int[][] grid;
+  int[] colHeight = { 0, 0, 0, 0, 0, 0 };
+  public int turn;
+  public int player;
+  public int winner;
+  public char[] icons = { ' ', 'R', 'B' };
+  Scanner scanner;
+
+  public ConnectFour() {
     name = "ConnectFour";
     description = "For 2 players. Not yet implemented.";
   }
-  
-  public void init(){
-    //Set variables to initial values for a new game
+
+  public void init() {
+    // Set variables to initial values for a new game
+    turn = 1;
+    player = 1;
+    gameover = false;
+    winner = 0;
+    scanner = new Scanner(System.in);
+    grid = new int[6][7];
+    for (int i = 0; i < 6; i++) {
+      for (int j = 0; j < 7; j++) {
+        grid[i][j] = 0;
+      }
+    }
   }
 
-  public void render(){
-    //draw the grid to the screen
+  public void render() {
+    // draw the grid to the screen
+
+    // clear screen
+    String display = "\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n";
+    // title
+    display += "Noughts and Crosses\r\n";
+    display += "____________________________\r\n\r\n";
+    // play info
+    if (gameover && winner == 0) {
+      display += "Game over. It was a draw.\r\n";
+    } else if (gameover) {
+      display += "Game over. The winner is " + icons[winner] + "\r\n";
+    } else {
+      display += "Turn: " + turn + " Current Player: " + icons[player] + "\r\n";
+    }
+    display += "____________________________\r\n";
+    // grid
+    display += "| " + icons[grid[0][0]] + " | " + icons[grid[1][0]] + " | " + icons[grid[2][0]] + " | "
+        + icons[grid[3][0]] + " | " + icons[grid[4][0]] + " | " + icons[grid[5][0]] + " |\r\n";
+    display += "|___|___|___|___|___|___|\r\n";
+
+    display += "| " + icons[grid[0][1]] + " | " + icons[grid[1][1]] + " | " + icons[grid[2][1]] + " | "
+        + icons[grid[3][1]] + " | " + icons[grid[4][1]] + " | " + icons[grid[5][1]] + " |\r\n";
+    display += "|___|___|___|___|___|___|\r\n";
+
+    display += "| " + icons[grid[0][2]] + " | " + icons[grid[1][2]] + " | " + icons[grid[2][2]] + " | "
+        + icons[grid[3][2]] + " | " + icons[grid[4][2]] + " | " + icons[grid[5][2]] + " |\r\n";
+    display += "|___|___|___|___|___|___|\r\n";
+
+    display += "| " + icons[grid[0][3]] + " | " + icons[grid[1][3]] + " | " + icons[grid[2][3]] + " | "
+        + icons[grid[3][3]] + " | " + icons[grid[4][3]] + " | " + icons[grid[5][3]] + " |\r\n";
+    display += "|___|___|___|___|___|___|\r\n";
+
+    display += "| " + icons[grid[0][4]] + " | " + icons[grid[1][4]] + " | " + icons[grid[2][4]] + " | "
+        + icons[grid[3][4]] + " | " + icons[grid[4][4]] + " | " + icons[grid[5][4]] + " |\r\n";
+    display += "|___|___|___|___|___|___|\r\n";
+
+    display += "| " + icons[grid[0][5]] + " | " + icons[grid[1][5]] + " | " + icons[grid[2][5]] + " | "
+        + icons[grid[3][5]] + " | " + icons[grid[4][5]] + " | " + icons[grid[5][5]] + " |\r\n";
+    display += "|___|___|___|___|___|___|\r\n";
+
+    display += "| " + icons[grid[0][6]] + " | " + icons[grid[1][6]] + " | " + icons[grid[2][6]] + " | "
+        + icons[grid[3][6]] + " | " + icons[grid[4][6]] + " | " + icons[grid[5][6]] + " |\r\n";
+    display += "|_1_|_2_|_3_|_4_|_5_|_6_|\r\n";
+    // play help
+    if (gameover) {
+      display += "Return to menu in 5 seconds.";
+    } else {
+      display += "Enter a number corresponding to an empty collumn.";
+    }
+    // print
+    System.out.println(display);
   }
 
-  public void input(){
-    //get the next move from the current player
+  public void input() {
+    // get the next move from the current player
+    int input = scanner.nextInt();
+    if (colHeight[input - 1] < 7) {
+      grid[input - 1][6 - colHeight[input - 1]] = player;
+      colHeight[input - 1] = colHeight[input - 1] + 1;
+      player = 3 - player;
+      turn++;
+    }
   }
 
-  public void update(){
-    //check if a player has won
+  public void update() {
+    // check if a player has won
   }
 
-  public void endgame(){
-    //run render to display an end game message, wait for a few seconds then terminate.
+  public void endgame() {
+    // run render to display an end game message, wait for a few seconds then
+    // terminate.
+    render();
+    try {
+      Thread.sleep(5000);
+    } catch (Exception e) {
+    }
   }
 }
--- Y12 Easter Project - ASCII Games/OandX.java
+++ Y12 Easter Project - ASCII Games/OandX.java
@@ -1,178 +1,183 @@
 import java.util.Scanner;
+
 class OandX extends Game {
 
   int[][] grid;
   public int turn;
   public int player;
   public int winner;
-  public char[] icons = {' ','O','X'};
+  public char[] icons = { ' ', 'O', 'X' };
   Scanner scanner;
 
-  public OandX(){
+  public OandX() {
     name = "Noughts and Crosses";
     description = "For 2 players. Try to make a line of threes 0s or three Xs in the grid.";
   }
 
-  public void init(){
+  public void init() {
     turn = 1;
     player = 1;
     gameover = false;
     winner = 0;
     scanner = new Scanner(System.in);
     grid = new int[4][4];
-    for (int i=0;i<4;i++){
-      for (int j=0;j<4;j++){
-        grid[i][j]=0;
+    for (int i = 0; i < 4; i++) {
+      for (int j = 0; j < 4; j++) {
+        grid[i][j] = 0;
       }
     }
   }
 
-  public void render(){
+  public void render() {
     String display = "\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n";
-    display+= "Noughts and Crosses\r\n";
-    display+= "____________________________\r\n";
-    if(gameover && winner==0){
-      display+="Game over. It was a draw.\r\n";
-    } else if(gameover){
-      display+="Game over. The winner is "+icons[winner]+"\r\n";
+    display += "Noughts and Crosses\r\n";
+    display += "____________________________\r\n";
+    if (gameover && winner == 0) {
+      display += "Game over. It was a draw.\r\n";
+    } else if (gameover) {
+      display += "Game over. The winner is " + icons[winner] + "\r\n";
     } else {
-    display+= "Turn: "+turn+" Current Player: "+icons[player]+"\r\n";
+      display += "Turn: " + turn + " Current Player: " + icons[player] + "\r\n";
     }
-    display+= "____________________________\r\n";
-    display+= "1  |2  |3  |4  \r\n";
-    display+= " "+icons[grid[0][0]]+" | "+icons[grid[0][1]]+" | "+icons[grid[0][2]]+" | "+icons[grid[0][3]]+" \r\n";
-    display+= "___|___|___|___\r\n";
-    display+= "q  |w  |e  |r  \r\n";
-    display+= " "+icons[grid[1][0]]+" | "+icons[grid[1][1]]+" | "+icons[grid[1][2]]+" | "+icons[grid[1][3]]+" \r\n";
-    display+= "___|___|___|___\r\n";
-    display+= "a  |s  |d  |f  \r\n";
-    display+= " "+icons[grid[2][0]]+" | "+icons[grid[2][1]]+" | "+icons[grid[2][2]]+" | "+icons[grid[2][3]]+" \r\n";
-    display+= "___|___|___|___\r\n";
-    display+= "z  |x  |c  |v  \r\n";
-    display+= " "+icons[grid[3][0]]+" | "+icons[grid[3][1]]+" | "+icons[grid[3][2]]+" | "+icons[grid[3][3]]+" \r\n";
-    display+= "   |   |   |\r\n";
-    display+= "____________________________\r\n";
-    if(gameover){
-      display+="Return to menu in 5 seconds.";
-    } else{
-      display+= "Enter a letter corresponding to an empty grid space.";
+    display += "____________________________\r\n";
+    
+    display += "1  |2  |3  |4  \r\n";
+    display += " " + icons[grid[0][0]] + " | " + icons[grid[0][1]] + " | " + icons[grid[0][2]] + " | " + icons[grid[0][3]] + " \r\n";
+    display += "___|___|___|___\r\n";
+    
+    display += "q  |w  |e  |r  \r\n";
+    display += " " + icons[grid[1][0]] + " | " + icons[grid[1][1]] + " | " + icons[grid[1][2]] + " | " + icons[grid[1][3]] + " \r\n";
+    display += "___|___|___|___\r\n";
+    
+    display += "a  |s  |d  |f  \r\n";
+    display += " " + icons[grid[2][0]] + " | " + icons[grid[2][1]] + " | " + icons[grid[2][2]] + " | " + icons[grid[2][3]] + " \r\n";
+    display += "___|___|___|___\r\n";
+    
+    display += "z  |x  |c  |v  \r\n";
+    display += " " + icons[grid[3][0]] + " | " + icons[grid[3][1]] + " | " + icons[grid[3][2]] + " | " + icons[grid[3][3]] + " \r\n";
+    display += "   |   |   |\r\n";
+    display += "____________________________\r\n";
+    if (gameover) {
+      display += "Return to menu in 5 seconds.";
+    } else {
+      display += "Enter a letter corresponding to an empty grid space.";
     }
     System.out.println(display);
   }
 
-  public void input(){
+  public void input() {
     char input = scanner.next().charAt(0);
-    switch (input){
+    switch (input) {
       case '1':
-        if(grid[0][0]==0){
-          grid[0][0]=player;
+        if (grid[0][0] == 0) {
+          grid[0][0] = player;
           player = 3 - player;
-          turn ++;
+          turn++;
         }
         break;
       case '2':
-        if(grid[0][1]==0){
-          grid[0][1]=player;
+        if (grid[0][1] == 0) {
+          grid[0][1] = player;
           player = 3 - player;
-          turn ++;
+          turn++;
         }
         break;
       case '3':
-        if(grid[0][2]==0){
-          grid[0][2]=player;
+        if (grid[0][2] == 0) {
+          grid[0][2] = player;
           player = 3 - player;
-          turn ++;
+          turn++;
         }
         break;
       case '4':
-        if(grid[0][3]==0){
-          grid[0][3]=player;
+        if (grid[0][3] == 0) {
+          grid[0][3] = player;
           player = 3 - player;
-          turn ++;
+          turn++;
         }
         break;
       case 'q':
-        if(grid[1][0]==0){
-          grid[1][0]=player;
+        if (grid[1][0] == 0) {
+          grid[1][0] = player;
           player = 3 - player;
-          turn ++;
+          turn++;
         }
         break;
       case 'w':
-        if(grid[1][1]==0){
-          grid[1][1]=player;
+        if (grid[1][1] == 0) {
+          grid[1][1] = player;
           player = 3 - player;
-          turn ++;
+          turn++;
         }
         break;
       case 'e':
-        if(grid[1][2]==0){
-          grid[1][2]=player;
+        if (grid[1][2] == 0) {
+          grid[1][2] = player;
           player = 3 - player;
-          turn ++;
+          turn++;
         }
         break;
       case 'r':
-        if(grid[1][3]==0){
-          grid[1][3]=player;
+        if (grid[1][3] == 0) {
+          grid[1][3] = player;
           player = 3 - player;
-          turn ++;
+          turn++;
         }
         break;
       case 'a':
-        if(grid[2][0]==0){
-          grid[2][0]=player;
+        if (grid[2][0] == 0) {
+          grid[2][0] = player;
           player = 3 - player;
-          turn ++;
+          turn++;
         }
         break;
       case 's':
-        if(grid[2][1]==0){
-          grid[2][1]=player;
+        if (grid[2][1] == 0) {
+          grid[2][1] = player;
           player = 3 - player;
-          turn ++;
+          turn++;
         }
         break;
       case 'd':
-        if(grid[2][2]==0){
-          grid[2][2]=player;
+        if (grid[2][2] == 0) {
+          grid[2][2] = player;
           player = 3 - player;
-          turn ++;
+          turn++;
         }
         break;
       case 'f':
-        if(grid[2][3]==0){
-          grid[2][3]=player;
+        if (grid[2][3] == 0) {
+          grid[2][3] = player;
           player = 3 - player;
-          turn ++;
+          turn++;
         }
         break;
       case 'z':
-        if(grid[3][0]==0){
-          grid[3][0]=player;
+        if (grid[3][0] == 0) {
+          grid[3][0] = player;
           player = 3 - player;
-          turn ++;
+          turn++;
         }
         break;
       case 'x':
-        if(grid[3][1]==0){
-          grid[3][1]=player;
+        if (grid[3][1] == 0) {
+          grid[3][1] = player;
           player = 3 - player;
-          turn ++;
+          turn++;
         }
         break;
       case 'c':
-        if(grid[3][2]==0){
-          grid[3][2]=player;
+        if (grid[3][2] == 0) {
+          grid[3][2] = player;
           player = 3 - player;
-          turn ++;
+          turn++;
         }
         break;
       case 'v':
-        if(grid[3][3]==0){
-          grid[3][3]=player;
+        if (grid[3][3] == 0) {
+          grid[3][3] = player;
           player = 3 - player;
-          turn ++;
+          turn++;
         }
         break;
       default:
@@ -180,26 +185,60 @@ class OandX extends Game {
     }
   }
 
-  public void update(){
-    if(grid[0][0]==grid[0][1] && grid[0][0]==grid[0][2] && grid[0][0]==grid[0][3] && grid[0][0]!=0){gameover = true;winner=grid[0][0];} //row 1
-    if(grid[1][0]==grid[1][1] && grid[1][0]==grid[1][2] && grid[1][0]==grid[1][3] && grid[1][0]!=0){gameover = true;winner=grid[1][0];} //row 2
-    if(grid[2][0]==grid[2][1] && grid[2][0]==grid[2][2] && grid[2][0]==grid[2][3] && grid[2][0]!=0){gameover = true;winner=grid[2][0];} //row 3
-    if(grid[3][0]==grid[3][1] && grid[3][0]==grid[3][2] && grid[3][0]==grid[3][3] && grid[3][0]!=0){gameover = true;winner=grid[3][0];} //row 4
+  public void update() {
+    if (grid[0][0] == grid[0][1] && grid[0][0] == grid[0][2] && grid[0][0] == grid[0][3] && grid[0][0] != 0) {
+      gameover = true;
+      winner = grid[0][0];
+    } // row 1
+    if (grid[1][0] == grid[1][1] && grid[1][0] == grid[1][2] && grid[1][0] == grid[1][3] && grid[1][0] != 0) {
+      gameover = true;
+      winner = grid[1][0];
+    } // row 2
+    if (grid[2][0] == grid[2][1] && grid[2][0] == grid[2][2] && grid[2][0] == grid[2][3] && grid[2][0] != 0) {
+      gameover = true;
+      winner = grid[2][0];
+    } // row 3
+    if (grid[3][0] == grid[3][1] && grid[3][0] == grid[3][2] && grid[3][0] == grid[3][3] && grid[3][0] != 0) {
+      gameover = true;
+      winner = grid[3][0];
+    } // row 4
 
-    if(grid[0][0]==grid[1][0] && grid[0][0]==grid[2][0] && grid[0][0]==grid[3][0] && grid[0][0]!=0){gameover = true;winner=grid[0][0];} //collum 1
-    if(grid[0][1]==grid[1][1] && grid[0][1]==grid[2][1] && grid[0][1]==grid[3][1] && grid[0][1]!=0){gameover = true;winner=grid[0][1];} //collum 2
-    if(grid[0][2]==grid[1][2] && grid[0][2]==grid[2][2] && grid[0][2]==grid[3][2] && grid[0][2]!=0){gameover = true;winner=grid[0][2];} //collum 3
-    if(grid[0][3]==grid[1][3] && grid[0][3]==grid[2][3] && grid[0][3]==grid[3][3] && grid[0][3]!=0){gameover = true;winner=grid[0][3];} //collum 4
+    if (grid[0][0] == grid[1][0] && grid[0][0] == grid[2][0] && grid[0][0] == grid[3][0] && grid[0][0] != 0) {
+      gameover = true;
+      winner = grid[0][0];
+    } // collum 1
+    if (grid[0][1] == grid[1][1] && grid[0][1] == grid[2][1] && grid[0][1] == grid[3][1] && grid[0][1] != 0) {
+      gameover = true;
+      winner = grid[0][1];
+    } // collum 2
+    if (grid[0][2] == grid[1][2] && grid[0][2] == grid[2][2] && grid[0][2] == grid[3][2] && grid[0][2] != 0) {
+      gameover = true;
+      winner = grid[0][2];
+    } // collum 3
+    if (grid[0][3] == grid[1][3] && grid[0][3] == grid[2][3] && grid[0][3] == grid[3][3] && grid[0][3] != 0) {
+      gameover = true;
+      winner = grid[0][3];
+    } // collum 4
 
-    if(grid[0][0]==grid[1][1] && grid[0][0]==grid[2][2] && grid[0][0]==grid[3][3] && grid[0][0]!=0){gameover = true;winner=grid[0][0];} //diagonal TL-BR
-    if(grid[0][3]==grid[1][2] && grid[0][3]==grid[2][1] && grid[0][3]==grid[3][0] && grid[0][3]!=0){gameover = true;winner=grid[0][3];} //diagonal TR-BL
-    if(turn==17){gameover = true;winner=0;}
+    if (grid[0][0] == grid[1][1] && grid[0][0] == grid[2][2] && grid[0][0] == grid[3][3] && grid[0][0] != 0) {
+      gameover = true;
+      winner = grid[0][0];
+    } // diagonal TL-BR
+    if (grid[0][3] == grid[1][2] && grid[0][3] == grid[2][1] && grid[0][3] == grid[3][0] && grid[0][3] != 0) {
+      gameover = true;
+      winner = grid[0][3];
+    } // diagonal TR-BL
+    if (turn == 17) {
+      gameover = true;
+      winner = 0;
+    }
   }
 
-  public void endgame(){
+  public void endgame() {
     render();
-    try{
+    try {
       Thread.sleep(5000);
-    } catch (Exception e){}
+    } catch (Exception e) {
+    }
   }
 }
